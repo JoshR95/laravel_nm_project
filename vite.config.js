4ebb54e05fb68076f24fb9c4b4e94ce181f74afe
JoshR95 <joshuarickards@hotmail.co.uk>
2025-03-18 12:19:14 +0000

    Revert "trying fixes for cpanel css issue"
    This reverts commit 82583f205af7cbf71f4c188897fe737c901e3899.

--- vite.config.js
+++ vite.config.js
@@ -6,25 +6,6 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            publicDirectory: "public",
         }),
     ],
-
-    // maybe not needed, cpanel fix
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        rollupOptions: {
-            output: {
-                manualChunks: {}
-            }
-        }
-    },
-    // maybe not needed, cpanel fix
-    server: {
-        https: false,
-        host: true,
-        strictPort: true,
-        port: 5173,
-    }
 });
